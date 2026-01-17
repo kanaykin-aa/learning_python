78651c55908d2210ffff2dcade147e1a8f274ce0
kanaykin-aa <44dwp6kssx@privaterelay.appleid.com>
2026-01-17 17:53:47 +0500

    упражнение 2

---
+++ mark lutz learning python/part 1/module1.py
@@ -0,0 +1,6 @@
+print('Hello World')
+x='1'
+exec(open('test part 1.py').read()) #чтение из другого файла
+import module1
+module1.x
+input() #чтобы запустить код с помощью клика
--- mark lutz learning python/part 1/test part 1.py
+++ mark lutz learning python/part 1/test part 1.py
@@ -1,3 +1,3 @@
 print("hello world") #упражнение 1 С:\...\part1>py Hello world
-
+"""В упражнении 2 нужно создать новый файл module1.py"""
 
